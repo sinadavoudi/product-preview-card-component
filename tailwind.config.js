cda9b8b1ef7cae58227da5234da4dbc42fcfcab1
SEA <sinadavoudi84@gmail.com>
2023-08-03 21:33:28 +0330

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./style.css",
+  './index.html',
+],
+  theme: {
+    screens: {
+      'mobile': '375px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
